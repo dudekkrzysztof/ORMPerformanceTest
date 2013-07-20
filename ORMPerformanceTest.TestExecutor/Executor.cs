411eb0ccc7a7e6d9ee25f037fc83efa7ef9d97cb
Krzysztof Dudek <krzysztof.dudek@hotmail.com>
2013-07-20 17:03:37 +0200

    Add TestExecutor. Main was rewritten to use Executor. Parameterization of Main to take one int argument - test repeat count.

---
+++ ORMPerformanceTest.TestExecutor/Executor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Massive;
+using ORMPerformanceTest.Tests;
+using ORMPerformanceTest.Tests.Ado;
+using ORMPerformanceTest.Tests.Dapper;
+using ORMPerformanceTest.Tests.EF;
+using ORMPerformanceTest.Tests.Helpers;
+using ORMPerformanceTest.Tests.Peta;
+using ORMPerformanceTest.Tests.Simple.Data;
+
+namespace ORMPerformanceTest.TestExecutor
+{
+    public class Executor
+    {
+        private readonly List<ITest> _tests;
+        private readonly int _runCount;
+        private List<TestResult> _insertResult;
+        private List<TestResult> _countResult;
+        private List<TestResult> _selectAllResult;
+        private List<TestResult> _selectPartResult;
+        private List<TestResult> _selectJoinResult;
+        private List<TestResult> _updateResult;
+        private List<TestResult> _deleteResult;
+        public Executor(int runCount = 5)
+        {
+            _tests = new List<ITest>
+            {
+                new TestEF(),
+                new TestAdo(),
+                new TestDapper(),
+                new TestMassive(),
+                new TestPeta(),
+                new TestSimpleData()
+            };
+            _runCount = runCount;
+        }
+
+        public List<TestResult> Run()
+        {
+            string connection = ConfigurationManager.ConnectionStrings["ORMTest"].ConnectionString;
+            List<TestResult> averageResults = new List<TestResult>(_tests.Count * 7);
+
+            foreach (var test in _tests)
+            {
+                _insertResult = new List<TestResult>(7 * _runCount);
+                _countResult = new List<TestResult>(7 * _runCount);
+                _selectAllResult = new List<TestResult>(7 * _runCount);
+                _selectPartResult = new List<TestResult>(7 * _runCount);
+                _selectJoinResult = new List<TestResult>(7 * _runCount);
+                _updateResult = new List<TestResult>(7 * _runCount);
+                _deleteResult = new List<TestResult>(7 * _runCount);
+                test.InitDataBase(connection);
+                for (int i = 0; i < _runCount; i++)
+                {
+                    _insertResult.Add(test.Insert(connection));
+                    _countResult.Add(test.Count(connection));
+                    _selectAllResult.Add(test.SelectAll(connection));
+                    _selectPartResult.Add(test.SelectPart(connection));
+                    _selectJoinResult.Add(test.SelectJoin(connection));
+                    _updateResult.Add(test.Update100(connection));
+                    _deleteResult.Add(test.Delete100(connection));
+                }
+                averageResults.Add(GetAverage(_insertResult));
+                averageResults.Add(GetAverage(_countResult));
+                averageResults.Add(GetAverage(_selectAllResult));
+                averageResults.Add(GetAverage(_selectPartResult));
+                averageResults.Add(GetAverage(_selectJoinResult));
+                averageResults.Add(GetAverage(_updateResult));
+                averageResults.Add(GetAverage(_deleteResult));
+            }
+
+            return averageResults;
+        }
+
+        private static TestResult GetAverage(List<TestResult> insertResult)
+        {
+            return new TestResult
+            {
+                AfectedRecord = (int)insertResult.Average(result => result.AfectedRecord),
+                Label = insertResult.First().Label,
+                TimeInMilisecond = (int)insertResult.Average(result => result.TimeInMilisecond)
+            };
+        }
+    }
+}
--- PerformanceTestConsole/Program.cs
+++ PerformanceTestConsole/Program.cs
@@ -1,126 +1,35 @@
 ﻿using System;
-using System.Configuration;
-using Massive;
-using ORMPerformanceTest.Tests.Ado;
-using ORMPerformanceTest.Tests.Dapper;
-using ORMPerformanceTest.Tests.EF;
-using ORMPerformanceTest.Tests.Helpers;
-using ORMPerformanceTest.Tests.Peta;
-using ORMPerformanceTest.Tests.Simple.Data;
+using ORMPerformanceTest.TestExecutor;
 
 namespace PerformanceTestConsole
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            string connection = ConfigurationManager.ConnectionStrings["ORMTest"].ConnectionString;
-            TestAdo ado = new TestAdo();
-            TestEF ef = new TestEF();
-            TestPeta peta = new TestPeta();
-            TestMassive massive = new TestMassive();
-            TestSimpleData simple = new TestSimpleData();
-            TestDapper dapper = new TestDapper();
-            TestResult result;
+            Executor executor = CreateExecutor(args);
+            DateTime start = DateTime.Now;
 
-            ef.InitDataBase(connection);
-            result = ef.Insert(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ef.Count(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ef.SelectAll(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ef.SelectPart(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ef.SelectJoin(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ef.Update100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ef.Delete100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
+            var result = executor.Run();
+            foreach (var testResult in result)
+            {
+                Console.WriteLine("Test: {0} take: {1} millisecond affected {3} rows, {2} row/second", testResult.Label, testResult.TimeInMilisecond, testResult.RowPerSec, testResult.AfectedRecord);
+            }
 
-            massive.InitDataBase(connection);
-            result = massive.Insert(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = massive.Count(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = massive.SelectAll(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = massive.SelectPart(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = massive.SelectJoin(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = massive.Update100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = massive.Delete100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-
-            peta.InitDataBase(connection);
-            result = peta.Insert(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = peta.Count(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = peta.SelectAll(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = peta.SelectPart(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = peta.SelectJoin(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = peta.Update100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = peta.Delete100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-
-            dapper.InitDataBase(connection);
-            result = dapper.Insert(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = dapper.Count(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = dapper.SelectAll(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = dapper.SelectPart(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = dapper.SelectJoin(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = dapper.Update100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = dapper.Delete100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-
-            simple.InitDataBase(connection);
-            result = simple.Insert(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = simple.Count(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = simple.SelectAll(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = simple.SelectPart(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = simple.SelectJoin(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = simple.Update100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = simple.Delete100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-
-            ado.InitDataBase(connection);
-            result = ado.Insert(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ado.Count(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ado.SelectAll(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ado.SelectPart(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ado.SelectJoin(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ado.Update100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-            result = ado.Delete100(connection);
-            Console.WriteLine("{1} {2} {3} Test: {0} trwal: {1} milisec co daje na sekunde {2}", result.Label, result.TimeInMilisecond, result.RowPerSec, result.AfectedRecord);
-
+            Console.WriteLine("Total time: {0} millisecond", (DateTime.Now - start).TotalMilliseconds);
             Console.ReadLine();
 
+        }
+
+        private static Executor CreateExecutor(string[] args)
+        {
+            if (args.Length == 0)
+                return new Executor();
+            int counter = 0;
+            int.TryParse(args[0], out counter);
+            if (counter < 1)
+                return new Executor();
+            return new Executor(counter);
         }
     }
 }
